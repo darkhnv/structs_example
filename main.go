62ebbec4d9acb4187b60d98bc45d2dca99812d55
darkhnv <darxanovahmadjon@gmail.com>
2024-03-01 12:58:35 +0500

    update

--- main.go
+++ main.go
@@ -7,18 +7,23 @@ import (
 )
 
 func main() {
+	// Initialize an empty user
 	u := user.User{}
 
+	// Get user data
 	err := u.GetUserData()
 	if err != nil {
 		log.Fatalf("Error getting user data: %v", err)
 	}
 
+	// Print user information
 	fmt.Println("User Information:")
 	u.PrintUserData()
 
+	// Create a new admin
 	admin := user.NewAdmin("admin@example.com", "admin1234")
 
+	// Print admin information
 	fmt.Println("\nAdmin Information:")
 	admin.PrintAdminData()
 }
--- user/user.go
+++ user/user.go
@@ -1,3 +1,5 @@
+// user.go
+
 package user
 
 import (
@@ -6,41 +8,49 @@ import (
 	"time"
 )
 
+// User represents a user's information
 type User struct {
-	FirstName string
-	LastName  string
-	Birthdate string
-	CreatedAt time.Time
+	FirstName string    // First name of the user
+	LastName  string    // Last name of the user
+	Birthdate string    // Birthdate of the user (MM/DD/YYYY)
+	CreatedAt time.Time // Time when the user was created
 }
 
+// Admin represents an administrator's information
 type Admin struct {
-	Email    string
-	Password string
-	User     User
+	Email    string // Email of the admin
+	Password string // Password of the admin
+	User     User   // Embedded User struct for admin's basic information
 }
 
+// GetUserData prompts the user to input their information
 func (u *User) GetUserData() error {
 	u.FirstName = getUserInput("Please enter your first name: ")
 	u.LastName = getUserInput("Please enter your last name: ")
 	u.Birthdate = getUserInput("Please enter your birthdate (MM/DD/YYYY): ")
 
+	// Check if all fields are provided
 	if u.FirstName == "" || u.LastName == "" || u.Birthdate == "" {
 		return errors.New("all fields are required")
 	}
 
+	// Record the time when the user data was created
 	u.CreatedAt = time.Now()
 
 	return nil
 }
 
+// PrintUserData prints the user's information
 func (u *User) PrintUserData() {
 	fmt.Printf("Name: %s %s, Birthdate: %s\n", u.FirstName, u.LastName, u.Birthdate)
 }
 
+// PrintAdminData prints the admin's information
 func (a *Admin) PrintAdminData() {
 	fmt.Printf("Name: %s %s, Birthdate: %s, Email: %s, Created At: %s\n", a.User.FirstName, a.User.LastName, a.User.Birthdate, a.Email, a.User.CreatedAt.Format(time.RFC3339))
 }
 
+// getUserInput prompts the user for input and returns the value
 func getUserInput(prompt string) string {
 	fmt.Print(prompt)
 	var value string
@@ -48,6 +58,7 @@ func getUserInput(prompt string) string {
 	return value
 }
 
+// NewAdmin creates a new admin with default values
 func NewAdmin(email, password string) Admin {
 	return Admin{
 		Email:    email,
